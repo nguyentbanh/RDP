530f5428ad33f13c93c71214081c147250385ef9
nguyentbanh <banhtuannguyen@gmail.com>
2021-01-12 10:39:25 -1000

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -10,13 +10,13 @@ sudo ./install.sh
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
-read -p "Paste Here: " CRP
-su - ALOK -c """$CRP"""
-printf 'Check https://remotedesktop.google.com/access/ \n\n'
-if sudo apt-get upgrade &> /dev/null
-then
-    printf "\n\nUpgrade Completed " >&2
-else
-    printf "\n\nError Occured " >&2
-fi
+#printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+#read -p "Paste Here: " CRP
+#su - ALOK -c """$CRP"""
+#printf 'Check https://remotedesktop.google.com/access/ \n\n'
+#if sudo apt-get upgrade &> /dev/null
+#then
+#    printf "\n\nUpgrade Completed " >&2
+#else
+#    printf "\n\nError Occured " >&2
+#fi
