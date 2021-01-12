643c90b8eff4353138684707a7d294e1b14b1192
nguyentbanh <banhtuannguyen@gmail.com>
2021-01-12 10:45:24 -1000

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-printf "Installing RDP Be Patience... " >&2
+printf "Installing SEED lab Be Patience... " >&2
 {
 curl -o src-cloud.zip https://seed.nyc3.cdn.digitaloceanspaces.com/src-cloud.zip
 sudo apt update 
@@ -10,13 +10,3 @@ sudo ./install.sh
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
-#printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
-#read -p "Paste Here: " CRP
-#su - ALOK -c """$CRP"""
-#printf 'Check https://remotedesktop.google.com/access/ \n\n'
-#if sudo apt-get upgrade &> /dev/null
-#then
-#    printf "\n\nUpgrade Completed " >&2
-#else
-#    printf "\n\nError Occured " >&2
-#fi
